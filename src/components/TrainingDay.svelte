7ead371d7fd3cad7be5de9902d8da026d8428558
Max <maxime.leszczynski@laposte.net>
2020-03-28 06:56:08 +0100

    Mise en place du gymscheduler

---
+++ src/components/TrainingDay.svelte
@@ -0,0 +1,39 @@
+<script>
+  import TraininDayDetails from "./TrainingDayDetails.svelte";
+  const nbOfDays = [
+    { id: 1, text: "One day", value: [1] },
+    { id: 2, text: "Two days", value: [1, 2] },
+    { id: 3, text: "Three days", value: [1, 2, 3] },
+    { id: 4, text: "Four days", value: [1, 2, 3, 4] },
+    { id: 5, text: "Five days", value: [1, 2, 3, 4, 5] },
+    { id: 6, text: "Six days", value: [1, 2, 3, 4, 5, 6] },
+    { id: 7, text: "Seven days", value: [1, 2, 3, 4, 5, 6, 7] }
+  ];
+
+  let nbDaysTrainingPerWeek;
+</script>
+
+<style>
+
+</style>
+
+<div>
+  <form>
+    <select bind:value={nbDaysTrainingPerWeek}>
+      {#each nbOfDays as day (day.id)}
+        <option value={day}>{day.text} per week</option>
+      {/each}
+    </select>
+  </form>
+  {JSON.stringify(nbDaysTrainingPerWeek)}
+</div>
+
+{#if nbDaysTrainingPerWeek}
+  <div>
+    {#each nbDaysTrainingPerWeek.value as dayNumber, i}
+      <div>
+        <TraininDayDetails title={`day ${++i}`} />
+      </div>
+    {/each}
+  </div>
+{/if}
---
+++ src/components/TrainingDayDetails.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let title;
+  let exercise = {
+    activity: "",
+    sets: 4,
+    reps: 10
+  };
+
+  let exercises = [];
+</script>
+
+<div>
+  <h2>{title}</h2>
+</div>
